f1eb549d2942dd6df280f499beb6f8c8c5928d09
mariuszniepytalski <mniepytalski@gmail.com>
2023-10-27 21:29:51 +0200

    szukanie liczb pierwszych

---
+++ venv/liczby_pierwsze.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+start = datetime.now()
+current_time = start.strftime("%H:%M:%S")
+
+pierwsze = [2, 3]
+
+liczba = 5
+
+while liczba < 100000:
+
+    polowa = liczba / 2
+
+    sprawdzenie = 1
+    for pierwsza in pierwsze:
+
+        if pierwsza > polowa:
+            break
+
+        reszta = liczba % pierwsza
+        if reszta == 0:
+            sprawdzenie = 0
+            break
+
+    if sprawdzenie == 1:
+        pierwsze.append(liczba)
+
+    liczba += 2
+
+print(pierwsze)
+
+print("Start Time =", current_time)
+end = datetime.now()
+stop_time = end.strftime("%H:%M:%S")
+print("Stop Time =", stop_time)
+print("time = ", end - start)
+print("find:{} numbers".format(len(pierwsze)))
